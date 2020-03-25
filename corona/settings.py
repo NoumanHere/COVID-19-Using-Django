d2135b14aa47182b95d9b317850906b615713e57
Muhammad Nouman <59723761+NoumanHere@users.noreply.github.com>
2020-03-25 09:47:34 +0500

    Update settings.py

--- corona/settings.py
+++ corona/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ntwkph+ltstsp0dteu@9rt%jtn+rx)zry18l-!kj8$jd@_8^9^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['corona9.herokuapp.com']
 
 
 # Application definition
